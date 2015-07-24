eb728f7f66d45fd0f7ddc33b596f26bd6ea97c72
lenovo <ccnujsj@126.com>
2015-07-24 15:38:54 +0800

    求最大连续子数组和，蛮力法求解

--- max_sequence1.c
+++ max_sequence1.c
@@ -1,12 +1,11 @@
 /*穷举法，算出每个i~j之和，取最大值,i,j的for循环用来遍历序列a[i..j]，k的for 循环用来计算子序列[i..j]的和*/
 #include<stdio.h>
-int main(){
-	int a[10]={5,-5,3,-7,9,4,-3,7,-3,5};
+int MaxSequence(int a[],int n){
 	int i,j,k;
 	int max=0;
 	int max1,max2;
-	for (i=0;i<10;i++){
-		for (j=i;j<10;j++){//此处j从0开始也是可以的，但是会多些运算，应为在之前已经算过了
+	for (i=0;i<n;i++){
+		for (j=i;j<n;j++){//此处j从0开始也是可以的，但是会多些运算，因为在之前已经算过了
 			int sum=0;
 			for (k=i;k<=j;k++){
 				sum+=a[k];
@@ -20,4 +19,9 @@ int main(){
 	}
 	printf("最大连续子数组是从%d到%d，他们的和是%d\n",max1,max2,max);
 	return max;
+}
+int main(){
+	
+	int a[10]={5,-5,3,-7,9,4,-3,7,-3,5};
+	int max=MaxSequence(a,10);
 }
